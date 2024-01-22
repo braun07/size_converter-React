0e35ba4ac0dbd699bfc5c28cef5a930a38687ff0
braun07 <joaovitorbraun6@gmail.com>
2024-01-22 20:49:43 -0300

    feat: layout base

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,11 +1,21 @@
 import { useState } from 'react';
 
 function App() {
+  function SubmitForm() {
+    console.log('teste');
+  }
 
   return (
     <>
       <div id="SizeMain">
-        <input type="number" />
+        <header>
+          <span>Welcome to my</span>
+          <h1>Size Converter's</h1>
+        </header>
+
+        <form onSubmit={SubmitForm} className='form'>
+
+        </form>
       </div>
     </>
   )
